76afb8f3b041d5715110ec4dc796a197ff97be14
djskl <448189234@qq.com>
2018-03-25 10:33:06 +0800

    修改文件输出路径

--- crawl_lianjia_zs.sh
+++ crawl_lianjia_zs.sh
@@ -2,7 +2,7 @@
 ndir=$(cd `dirname $0`; pwd)
 cd $ndir
 
-filepath=$ndir/$(date +%Y%m%d)_zs_lj.csv
+filepath=$ndir/data/lianjia/zs/$(date +%Y%m%d)_zs_lj.csv
 
 /usr/bin/python2 /usr/bin/scrapy crawl lianjia_for_sale -t lianjia_csv -o $filepath -L INFO >& $ndir/$(date +%Y%m%d)_zs_lj.logs
 
